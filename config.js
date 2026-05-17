7000419ae598fc20d0d6988d5d01f20b9367e51f
ablaye2020 <163043278+ablaye2020@users.noreply.github.com>
2026-05-17 11:31:38 +0000

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,12 @@
+export default {
+  BotName: "J O Y ~ M D",
+  nameCreator: "JOYBOY",
+  Channel: "https://whatsapp.com/channel/0029VbBGPvF8aKvKN8YQft2d",
+  ApiKey: "",
+  Newsletter: "120363425431833423@newsletter",
+  Newsletter2: "120363425431833423@newsletter",
+  Newsletter3: "120363425431833423@newsletter",
+  Newsletter4: "120363425431833423@newsletter",
+  Newsletter5: "120363425431833423@newsletter",
+  Newsletter6: "120363425431833423@newsletter"
+};
